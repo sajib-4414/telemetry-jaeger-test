a190fbff2d626609c4760113d1acc08f29a1a0b7
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2025-03-15 21:44:04 -0600

    telemetry configuration working

--- app2/src/main/java/com/sajib/opentelemetry/opennn/Controller.java
+++ app2/src/main/java/com/sajib/opentelemetry/opennn/Controller.java
@@ -1,21 +1,18 @@
 package com.sajib.opentelemetry.opennn;
 
-import io.micrometer.tracing.Tracer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("service")
 public class Controller {
     private RestTemplate restTemplate;
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
-
 
     @Value("${spring.application.name}")
     private String applicationName;
@@ -28,7 +25,7 @@ public class Controller {
     public ResponseEntity path1() {
 
         logger.info("Incoming request at {} for request /path1 ", applicationName);
-        String response = restTemplate.getForObject("http://localhost:8086/service/path2", String.class);
+        String response = restTemplate.getForObject("http://localhost:8090/service/path2", String.class);
         return ResponseEntity.ok("response from /path1 + " + response);
     }
 
